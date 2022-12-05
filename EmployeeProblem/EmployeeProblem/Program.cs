74b0c59ec55a26fbcd9e111fcba916407c15f294
ajeet1010 <ajeetpal101999@gmail.com>
2022-12-06 02:17:20 +0530

    Salary

--- EmployeeProblem/EmployeeProblem/Program.cs
+++ EmployeeProblem/EmployeeProblem/Program.cs
@@ -13,7 +13,7 @@ namespace EmployeeProblem
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Choose the option to perform the operation:-  1) Attendance \n " +
+                Console.WriteLine("Choose the option to perform the operation:-  1) Attendance \n 2) Salary of Employee " +
                     "Enter the no to operate: ");
                 int option = Convert.ToInt32(Console.ReadLine());
 
@@ -22,6 +22,10 @@ namespace EmployeeProblem
                     case 1:
                         Attendance attendance = new Attendance();
                         attendance.Preabs();
+                        break;
+                    case 2:
+                        Wage wage = new Wage();
+                        wage.Salary();
                         break;
 
                 }
