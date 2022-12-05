5293b33a0deafe797f94775edcb012be9fec9332
ajeet1010 <ajeetpal101999@gmail.com>
2022-12-06 02:11:00 +0530

    Attendance

---
+++ EmployeeProblem/EmployeeProblem/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeProblem
+{
+    public class EmployeeProblem
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Choose the option to perform the operation:-  1) Attendance \n " +
+                    "Enter the no to operate: ");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Attendance attendance = new Attendance();
+                        attendance.Preabs();
+                        break;
+
+                }
+            }
+        }
+    }
+}
